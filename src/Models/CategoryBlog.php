d364e97067d0b141f1c112bce2c4bdf5341d6fc9
giauphan <giauphan012@gmail.com>
2024-01-18 16:32:58 +0700

    update model CategoryBlog

--- src/Models/CategoryBlog.php
+++ src/Models/CategoryBlog.php
@@ -3,8 +3,13 @@
 namespace Giauphan\CrawlBlog\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CategoryBlog extends Model
 {
     protected $guarded = [];
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
